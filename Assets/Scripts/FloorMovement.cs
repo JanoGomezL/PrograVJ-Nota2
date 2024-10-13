09a2abeced4168d39d0aa9bb8d980c9fed174c08
JanoGomezL <20190857@aloe.ulima.edu.pe>
2024-10-13 13:50:35 -0500

    Update FloorMovement.cs

--- Assets/Scripts/FloorMovement.cs
+++ Assets/Scripts/FloorMovement.cs
@@ -7,11 +7,24 @@ public class FloorMovement : MonoBehaviour
     [SerializeField]
     private PlayerMovement m_PlayerMovement;
 
+    [SerializeField]
+    private Transform m_Sprite;
+
     private void OnCollisionEnter2D(Collision2D other) 
     {
         if (other.collider.CompareTag("Player"))
         {
             m_PlayerMovement.DeactivateJump();
+        }
+    }
+
+    private void Update()
+    {
+        if (m_Sprite != null)
+        {
+            Vector3 spritePosition = m_Sprite.position;
+            spritePosition.x = transform.position.x;
+            m_Sprite.position = spritePosition;
         }
     }
 }
